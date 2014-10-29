69640ac34065819b86ba031e967773e2665a1c7d
eanplatter <eanplatter@gmail.com>
2014-10-29 16:33:04 -0600

    first commit

---
+++ liardatabase.js
@@ -0,0 +1,16 @@
+var friends = [
+  {
+    name: 'Josh',
+    age: 'ineffible',
+    level: 4
+  },
+  {
+    name: 'Tyler',
+    age: 24,
+    level: 6
+  }
+]
+
+module.exports.getFriends = function() {
+  return friends;
+}
---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var database = require('./liardatabase');
+var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+
+var app = express();
+
+
+//CONNECTION
+var connection = new Sequelize('calcWatches', 'root', null, 
+  {
+    host: 'localhost'
+  }
+);
+
+// MODEL
+var Watch = connection.define('watch', {
+  color: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  brand: Sequelize.STRING
+})
+
+
+// ENDPOINT
+app.get('/watches', function(req, res) {
+  Watch.all().then(function(data) {
+    res.send(data);
+  });
+});
+
+try {
+  connection.sync();
+}
+
+catch(err) {
+ console.log('database error:' + err);
+}
+
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
